1cf53cfeced14723b7cbd9febbb9db4ab6b07b03
Martin Frigaard <mjfrigaard@pm.me>
2023-11-19 14:38:25 -0800

    launch()

--- R/launch_app.R
+++ R/launch_app.R
@@ -10,12 +10,24 @@
 #' # launch_app("02_movies-app")
 launch_app <- function(app) {
 
-  clone_app(app = app, open = FALSE)
+  # keep track
+  original_dir <- getwd()
 
-  setwd(app)
+  # get current folders
+  current_dirs <- list.dirs(path = original_dir,
+                            full.names = FALSE)
 
-  withr::local_options(list(shiny.autoload.r = FALSE))
+  if (any(app %in% current_dirs)) {
+    setwd(app)
+    withr::local_options(list(shiny.autoload.r = FALSE))
+    shiny::runApp()
+  } else {
+    clone_app(app = app, open = FALSE)
+    setwd(app)
+    withr::local_options(list(shiny.autoload.r = FALSE))
+    shiny::runApp()
+  }
 
-  shiny::runApp()
+  setwd(original_dir)
 
 }
